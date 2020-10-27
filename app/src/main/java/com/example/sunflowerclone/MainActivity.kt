9c83055f410c293010c516befe5061a04bc7116b
김초희 <qwerpoiu1316@naver.com>
2020-10-28 00:48:04 +0900

    Add : Inflate activity_main.xml file to MainActivity.kt 1. activity_main.xml 파일을 구성함 2. MainAcitivty 파일에 data binding을 사용하여 inflate함

--- app/src/main/java/com/example/sunflowerclone/MainActivity.kt
+++ app/src/main/java/com/example/sunflowerclone/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.sunflowerclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.sunflowerclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
