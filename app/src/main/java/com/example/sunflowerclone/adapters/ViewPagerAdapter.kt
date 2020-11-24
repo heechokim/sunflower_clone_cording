5d8265fa2556510b916dba7cc6381d6ce99211f3
Kimchohee <chchgml10@gmail.com>
2020-11-25 01:21:34 +0900

    2020/11/25 Implement ViewPagerAdapter

--- app/src/main/java/com/example/sunflowerclone/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/sunflowerclone/adapters/ViewPagerAdapter.kt
@@ -5,17 +5,23 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.sunflowerclone.Fragment1
 import com.example.sunflowerclone.Fragment2
 
+const val MY_GARDEN_PAGE_INDEX = 0
+const val PLANT_LIST_PAGE_INDEX = 1
+
 class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    // Init map that match fragment to index
+    private val fragmentsCreator: Map<Int, () -> Fragment> = mapOf(
+        // TODO(Fragment1(), Fragment2()는 임시 프래그먼트므로 제대로된 프래그먼트 생성 후 이름 변경해야)
+        MY_GARDEN_PAGE_INDEX to { Fragment1() },
+        PLANT_LIST_PAGE_INDEX to { Fragment2() }
+    )
+
     override fun getItemCount(): Int {
-        return 2
+        return fragmentsCreator.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        if(position == 0) {
-            return Fragment1()
-        }else{
-            return Fragment2()
-        }
-        //return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
+        return fragmentsCreator[position]?.invoke() ?: throw IndexOutOfBoundsException()
     }
 }
